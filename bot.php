ce84dedae481285c0784882f2145faa772676bcb
Trevor Lemeron <trevorrl@me.com>
2017-07-31 16:57:25 -0400

    added logic for sPoNgEbOb TeXt replies when anybody asks a question

--- bot.php
+++ bot.php
@@ -4,8 +4,6 @@ namespace Bot;
 
 include __DIR__.'/vendor/autoload.php';
 
-use Discord\DiscordCommandClient;
-
 $token = file_get_contents('token');
 
 $discord = new \Discord\Discord([
@@ -15,7 +13,7 @@ $discord = new \Discord\Discord([
 $discord->on('ready', function ($discord) {
 	echo "Node Load Patrol is Ready.", PHP_EOL;
 
-	// Listening for events
+	// triggers for new messages
 	$discord->on('message', function ($message) {
 		$content = strtolower($message->content);
 
@@ -33,18 +31,45 @@ $discord->on('ready', function ($discord) {
 		'ICQ'
 		);
 
-		if (strpos($content, "discord") !== false) {
-			// If someone says 'discord', reply with "@user, get in the [blank]" and echo "COMMAND TRIGGERED"
+		if (strpos($content, "discord") !== false && $message->author->username !== "node-lode-patrol") {
+			// If someone says 'discord', reply with "@user, get in the [blank]"
 			$choice = $choices[array_rand($choices, 1)];
 			$message->channel->sendMessage("{$message->author}, get in the {$choice}", true);
-		} elseif (strpos($content, "tony cannoli") !== false) {
-			// If someone says 'tony cannoli', reply with this picture and echo "COMMAND TRIGGERED"
-			$message->channel->sendMessage("http://i.imgur.com/VLb8J4U.jpg");
-		} elseif (strpos($content, "brass monkey") !== false) {
+		} elseif (strpos($content, "tony cannoli") !== false && $message->author->username !== "node-lode-patrol") {
+			// If someone says 'tony cannoli', reply with this picture and say a thing
+			$message->channel->sendMessage("Tony Cannoli, Tony Cannoli, get in the ravioli.", true);
+            sleep(1);
+            $message->channel->sendMessage("http://i.imgur.com/VLb8J4U.jpg");
+		} elseif (strpos($content, "brass monkey") !== false && $message->author->username !== "node-lode-patrol") {
 			// if someone says 'brass monkey', respond with "That funky monkey!" and this gif
             $message->channel->sendMessage("That funky monkey! \n http://i.imgur.com/JQg8OOM.gif");
-		} else {
-            // don't do anything
+		} elseif (strpos($content, "?") !== false && $message->author->username !== "node-lode-patrol") {
+            // if someone asks a question, reply in sPoNgEbOb TeXt
+            $str1 = $content;
+            $str2 = "";
+            $index = 0;
+            $upper = true;
+            // iterate over whole string
+            while (strlen($str2) < strlen($str1)) {
+                $nextChar = $str1[$index];
+                // if the next char is alpha, make it upper or lower and add it to $str2; otherwise, just add it to $str2
+                if (ctype_alpha($nextChar)) {
+                    if ($upper) {
+                        $nextChar = strtolower($nextChar);
+                        $upper = false;
+                    } else {
+                        $nextChar = strtoupper($nextChar);
+                        $upper = true;
+                    }
+                $str2 = $str2 . $nextChar;
+                $index++;
+                } else {
+                    $nextChar = $str1[$index];
+                    $str2 = $str2 . $nextChar;
+                    $index++;
+                }
+            }
+            $message->channel->sendMessage("{$str2}");
         };
 	});
 });
